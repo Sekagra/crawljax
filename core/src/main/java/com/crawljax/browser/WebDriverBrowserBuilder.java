cb5fde18d03e3fed75d3a5231bc9b58fff2cce85
Christoph Rudolf <christoph.rudolf@tum.de>
2018-05-23 18:45:40 +0200

    Added SSL support again, removed error.

--- core/src/main/java/com/crawljax/browser/WebDriverBrowserBuilder.java
+++ core/src/main/java/com/crawljax/browser/WebDriverBrowserBuilder.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +25,6 @@ import org.slf4j.LoggerFactory;
 public class WebDriverBrowserBuilder implements Provider<EmbeddedBrowser> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(WebDriverBrowserBuilder.class);
-	private static final Object CapabilityType = ;
 	private final CrawljaxConfiguration configuration;
 	private final Plugins plugins;
 
